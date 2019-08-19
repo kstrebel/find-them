0dec655b134200a53a18ac912d6e525efa0f28cf
Kathy Strebel <kthstrebel4@gmail.com>
2019-08-19 03:11:38 -0700

    Start on bomb logic

---
+++ Find Them/Assets/Scripts/Orb_Logic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orb_Logic : MonoBehaviour
+{
+    private bool isBomb = false;
+
+    public bool IsBomb
+    {
+        get { return isBomb; }
+        set { isBomb = value; }
+    }
+}
